1c4854378db0814bbd5a0541a0a3ca6d602b6665
Samuel Jansem <samuel.jansem@gmail.com>
2024-12-15 16:05:32 -0300

    ch03

--- ch03/variables/src/main.rs
+++ ch03/variables/src/main.rs
@@ -1,3 +1,25 @@
 fn main() {
-    println!("Hello, world!");
+    let x = 5;
+    println!("The value of x is: {}", x);
+    // x = 6; // Cannot assign twice to immutable variable
+    println!("The value of x is: {}", x);
+
+    // Shadowing
+    /*
+    We can shadow a variable to a different type, but when using mut, we can't change the type.
+    */
+
+    let x = 5;
+
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of x in inner scope is: {}", x);
+    }
+
+    println!("The value of x is: {}", x);
+
+    // let mut spaces = "   ";
+    // spaces = spaces.len(); // expected `&str`, found `usize`
 }
